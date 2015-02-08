6807a7fb668318f1527ebf997a5b2e26d1dc27ae
Pascal Mayer <laxap4@gmail.com>
2015-02-08 20:36:49 +0100

    class current added to CUR nav items

--- Initialisation/Files/theme/typoscript/nav/setup.ts
+++ Initialisation/Files/theme/typoscript/nav/setup.ts
@@ -1,21 +1,25 @@
 /* ---------------------------------------------------------------
- * Twitter bootstrap style navbar dropdown (with responsive mode)
+ * Navbar dropdown
  */
+
+# mobile nav button
+lib.mobileNavBtn = TEXT
+lib.mobileNavBtn {
+    wrap = <div class="navbar-header"><button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainnavbar">|</button></div>
+    # glyphicon from bootstrap
+    value = <span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+    # fontawesome
+    #value = <span class="fa fa-bars"></span>
+}
+
+# navbar
 lib.navbar = COA
 lib.navbar.wrap = <nav class="navbar navbar-default" role="navigation">|</nav>
 lib.navbar {
+    # mobile trigger
+    10 < lib.mobileNavBtn
 
-    # Responsive Navbar Part 1
-    10 = TEXT
-    10 {
-        # fontawesome
-        #value = <span class="fa fa-bars"></span>
-        # glyphicon from bootstrap
-        value = <span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
-        wrap = <div class="navbar-header"><button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mainnavbar">|</button></div>
-    }
-
-    # Responsive Navbar Part 2: Place all navbar contents you want collapsed withing .navbar-collapse.collapse.
+    # navigation
     20 = HMENU
     20.wrap = <div class="collapse navbar-collapse" id="mainnavbar"><ul class="nav navbar-nav">|</ul></div>
     20 {
@@ -29,24 +33,30 @@ lib.navbar {
             NO = 1
             NO.allWrap >
             NO.wrapItemAndSub = <li>|</li>
+            ACT = 1
+            ACT < .NO
+            ACT.wrapItemAndSub = <li class="active">|</li>
             CUR = 1
             CUR < .NO
-            CUR.wrapItemAndSub = <li class="active">|</li>
-            ACT = 1
-            ACT < .CUR
+            #CUR.ATagParams =  id="current"
+            CUR.wrapItemAndSub = <li class="active current">|</li>
 
             # Dropdown menu
             IFSUB = 1
             IFSUB < .NO
             IFSUB.wrapItemAndSub = <li class="dropdown">|</li>
             IFSUB.ATagParams = class="dropdown-toggle" role="button" data-toggle="dropdown" data-target="#"
+            #IFSUB.ATagParams = class="dropdown-toggle disabled" role="button" data-toggle="dropdown" data-target="#"
             IFSUB.ATagBeforeWrap = 1
             IFSUB.stdWrap.wrap = |<b class="caret"></b>
-            CURIFSUB = 1
-            CURIFSUB < .IFSUB
-            CURIFSUB.wrapItemAndSub = <li class="dropdown active">|</li>
+
             ACTIFSUB = 1
-            ACTIFSUB < .CURIFSUB
+            ACTIFSUB < .IFSUB
+            ACTIFSUB.wrapItemAndSub = <li class="dropdown active">|</li>
+
+            CURIFSUB = 1
+            CURIFSUB < .ACTIFSUB
+            CURIFSUB.wrapItemAndSub = <li class="dropdown active current">|</li>
         }
 
 
@@ -60,9 +70,10 @@ lib.navbar {
             NO.wrapItemAndSub = <li>|</li>
             CUR = 1
             CUR < .NO
-            CUR.wrapItemAndSub = <li class="active">|</li>
+            CUR.wrapItemAndSub = <li class="active current">|</li>
             ACT = 1
-            ACT < .CUR
+            ACT < .NO
+            ACT.wrapItemAndSub = <li class="active">|</li>
 
             IFUSB < .1.IFSUB
             CURIFSUB < .1.CURIFSUB
@@ -84,9 +95,8 @@ lib.navbar {
 
 
 /* ---------------------------------------------------------------
- * Additional navigations
+ * Breadcrump
  */
-
 lib.breadcrumb = COA
 lib.breadcrumb {
   20 = HMENU
@@ -110,12 +120,25 @@ lib.breadcrumb {
   }
 }
 
+
+/* ---------------------------------------------------------------
+ * Subnav
+ */
 lib.subnav = COA
-lib.subnav.wrap = <ul class="nav nav-list" id="sidebar-nav">|</ul>
 lib.subnav {
+    wrap = <ul class="nav nav-list" id="sidebar-nav">|</ul>
+
     10 = TEXT
     10.wrap = <li class="list-group-item">|</li>
+    #10.wrap {
+    #    override = <li class="list-group-item current">|</li>
+    #    override.if {
+    #        value.data = field:uid
+    #        equals.data = leveluid:1
+    #    }
+    #}
     10.data = leveltitle:1
+    #10.data =  levelfield:1,nav_title // leveltitle:1
     10.typolink.parameter.data = leveluid:1
 
     20 = HMENU
@@ -132,9 +155,10 @@ lib.subnav {
             NO.wrapItemAndSub = <li class="list-group-item">|</li>
             CUR = 1
             CUR < .NO
-            CUR.wrapItemAndSub = <li class="list-group-item active">|</li>
+            CUR.wrapItemAndSub = <li class="list-group-item active current">|</li>
             ACT = 1
             ACT < .CUR
+            ACT.wrapItemAndSub = <li class="list-group-item active">|</li>
 
             # Dropdown menu
             IFSUB = 1
@@ -145,7 +169,7 @@ lib.subnav {
 
             CURIFSUB = 1
             CURIFSUB < .IFSUB
-            CURIFSUB.wrapItemAndSub = <li class="list-group-item active">|</li>
+            CURIFSUB.wrapItemAndSub = <li class="list-group-item active current">|</li>
 
             ACTIFSUB = 1
             ACTIFSUB < .IFSUB
@@ -161,11 +185,12 @@ lib.subnav {
             NO = 1
             NO.allWrap >
             NO.wrapItemAndSub = <li class="list-group-item">|</li>
-            CUR = 1
-            CUR < .NO
-            CUR.wrapItemAndSub = <li class="list-group-item active">|</li>
             ACT = 1
-            ACT < .CUR
+            ACT < .NO
+            ACT.wrapItemAndSub = <li class="list-group-item active">|</li>
+            CUR = 1
+            CUR < .ACT
+            CUR.wrapItemAndSub = <li class="list-group-item active current">|</li>
 
             IFUSB < .1.IFSUB
             ACTIFSUB < .1.ACTIFSUB
@@ -177,13 +202,17 @@ lib.subnav {
             SPC.allWrap = <li class="divider"></li>
         }
 
-        3 > .2
+        3 < .2
         # no submenus anymore
         3.IFSUB >
         3.ACTIFSUB >
     }
 }
 
+
+/* ---------------------------------------------------------------
+ * Meta Nav
+ */
 lib.metanav = COA
 lib.metanav {
     20 = HMENU
